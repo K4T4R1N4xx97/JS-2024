e54b52d7e56ed2d6dc64b9635d8f60ffadcb19e1
unknown <katarina.sokolic97@gmail.com>
2024-03-12 20:34:10 +0100

    JS task

--- vjezba2/script.js
+++ vjezba2/script.js
@@ -6,5 +6,25 @@ const trenutaGod = 2024;
 /*string literal*/
 
 const osoba = "Ja sam"+ " " + ime + "," + (trenutaGod - godinaRod) + "-godišnji" + " " + zanimanje + ".";
+console.log(osoba);
 
-console.log(osoba)
+/* Template literal */
+/*varijable se pozivaju sa znbakom dolar i unutar viticastih zagrada*/ 
+
+const osobaNovo = `Ja sam ${ime}, ${trenutaGod - godinaRod}-godišnji ${zanimanje}`;
+console.log(osobaNovo);
+
+
+// \n\ služi da prelamanje u drugi red poput <br> u HTML-u
+console.log("String\n\ sa\n\ više\n\ linija");
+
+// ZADATAK
+
+const zemlja = "Hrvatska";
+const kontinent = "Europi";
+const populacija = 3.8e6;
+
+const recenica = `${zemlja} ima ${populacija} stanovnika i nalazi se u ${kontinent}`;
+console.log(recenica);
+
+console.log(zemlja.length);
