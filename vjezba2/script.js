9aa2fcc52e8c5cdbb3eb93da34760ced20f3ca2f
unknown <katarina.sokolic97@gmail.com>
2024-03-12 21:00:46 +0100

    last-JS-task

--- vjezba2/script.js
+++ vjezba2/script.js
@@ -28,3 +28,50 @@ const recenica = `${zemlja} ima ${populacija} stanovnika i nalazi se u ${kontine
 console.log(recenica);
 
 console.log(zemlja.length);
+
+/*Matematicke metode sa brojevima i varijablama*/
+
+let x;
+
+// izracun korjena broja
+// upise se Math. i onda on ponudi izracune
+x = Math.sqrt(16); // korijen broja
+x = Math.abs (5); // apsolutna vrijednost
+x = Math.round (5.2); // zaokruživanje na puni broj
+x = Math.ceil(5.2); // prisilno zaokruživanje na gore
+x = Math.floor(5.9); // prisilno zaokruživanje na dolje
+x = Math.trunc(4.4); // uklanjanje decimalne vrijednosti
+x = Math.min(2, 5, 6, 7, 1) // minimalni broj iz niza brojeva
+x = Math.max(2, 5, 6, 7, 1) // maksimalni broj iz niza brojeva
+x = Math.random(); // dobivanje random broja između 0 i 1
+
+
+console.log(x)
+
+//ZADATAK
+
+let randomBroj = Math.floor(Math.random() * 10 + 1);
+
+randomBroj = Math.trunc(Math.random() * 10 + 1);
+console.log(randomBroj);
+
+
+// ZADATAK 2
+
+let varijablaA = Math.trunc(Math.random() * 100 + 1); 
+let varijablaB = Math.trunc(Math.random() * 50 + 1);
+console.log(varijablaA, varijablaB);
+
+const zbroj = varijablaA + varijablaB;
+const oduzimanje = varijablaA - varijablaB;
+console.log(zbroj, oduzimanje);
+
+const primjer1 =`Zbroj varijable a i b je ${zbroj}`;
+const primjer2 =`Razlika varijable a i b je ${oduzimanje}`
+
+console.log(primjer1,'a', primjer2)
+
+
+
+
+
